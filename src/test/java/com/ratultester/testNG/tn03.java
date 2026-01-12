46053ab9ea1a2b5f291bda4f2beebd89ca7175f2
Ratul Nandy <ratul.tester@gmail.com>
2026-01-12 16:40:49 +0530

    ly added

---
+++ src/test/java/com/ratultester/testNG/tn03.java
@@ -0,0 +1,22 @@
+package com.ratultester.testNG;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class tn03 {
+
+    @Test
+    public void asserSH(){
+        SoftAssert sa = new SoftAssert();
+        sa.assertEquals("Ratul","Ratul","Not Matching");
+        System.out.println("End of the Soft Assert Program");
+        sa.assertAll();
+    }
+    @Test
+    public void asserHS(){
+        Assert.assertEquals("Ratul","Ratul","Not Equal");
+        Assert.assertEquals("Batul","Batul","Not Equal");
+        System.out.println("End of the Hard Assert Program");
+    }
+}
